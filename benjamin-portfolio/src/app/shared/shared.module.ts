fee542d3a7893b65d93deed1a13fec8a642c4d30
BenjaminBitcube <benjamin@bitcube.tech>
2022-11-01 20:18:08 +0200

    wip layout

--- benjamin-portfolio/src/app/shared/shared.module.ts
+++ benjamin-portfolio/src/app/shared/shared.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TitleComponent } from './title/title.component';
 import { SecondaryTitleComponent } from './secondary-title/secondary-title.component';
+import { PageContainerComponent } from './page-container/page-container.component';
+import { LayoutComponent } from './layout/layout.component';
+import { TextComponent } from './text/text.component';
 
-
+const components = [
+  TitleComponent, SecondaryTitleComponent, PageContainerComponent, LayoutComponent, TextComponent
+]
 
 @NgModule({
-  declarations: [TitleComponent, SecondaryTitleComponent],
+  declarations: components,
   imports: [
     CommonModule,
   ],
-  exports: [TitleComponent, SecondaryTitleComponent],
+  exports: components,
 })
 export class SharedModule { }
